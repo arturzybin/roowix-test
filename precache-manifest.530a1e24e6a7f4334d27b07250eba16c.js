9f9110592fa5b5b6fcc4fccc097c6b29b377b961
ArturZybin <artur.zybin.work@gmail.com>
2020-06-20 16:15:59 +0700

    Updates

---
+++ precache-manifest.530a1e24e6a7f4334d27b07250eba16c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8f9140d3824a9b38fdc8d98c5bc4fe2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7ddee5b301cec899087",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "cfc81b7d1171e4ce5a4d",
+    "url": "/static/css/main.3e35dc01.chunk.css"
+  },
+  {
+    "revision": "e7ddee5b301cec899087",
+    "url": "/static/js/2.8475c0d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8475c0d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfc81b7d1171e4ce5a4d",
+    "url": "/static/js/main.f3f6d1d9.chunk.js"
+  },
+  {
+    "revision": "6105aa1fd369a2e16d6a",
+    "url": "/static/js/runtime-main.ba6e6323.js"
+  }
+]);
